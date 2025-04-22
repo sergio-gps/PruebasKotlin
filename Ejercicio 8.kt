5d7f254c458a5da73ff0bca0c370b51e3f85e999
Sergio <spg147@inlumine.ual.es>
2025-04-22 13:41:34 +0200

    ejercicio sobre variables y su inicialización

---
+++ Ejercicio 8.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // Write your code below ☀️
+    var todaysDate: String = "22/04/2025"
+    println(todaysDate)
+  
+    var currentWeather: String
+    currentWeather = "soleado"
+    println(currentWeather)
+  }
