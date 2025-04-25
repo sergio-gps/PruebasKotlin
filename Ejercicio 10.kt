19de2ed4f44898f13fc163dae6bcb90ec074c668
Sergio <spg147@inlumine.ual.es>
2025-04-25 14:06:42 +0200

    ejercicio sobre inferencia de tipos y ejercicio de recapitulación

---
+++ Ejercicio 10.kt
@@ -0,0 +1,5 @@
+fun main() {
+    var typeTest = 2.6
+    // Declare your variable above ⬆️
+    print("${typeTest::class.simpleName}")    
+  }
---
+++ Ejercicio 11.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // Write your code below 
+    val favouriteSong: String = "Strobe - deadmau5"
+    val duration = 10.34
+    var isPlaying = false
+  
+    println(favouriteSong)
+    println(duration)
+    println(isPlaying)
+  }
