918bac5942e67e14b8b7d6eba0b283ba1dcbd9b7
Sergio <spg147@inlumine.ual.es>
2025-04-30 14:56:57 +0200

    ejercicio concatenación de stings

---
+++ Ejercicio 12.kt
@@ -0,0 +1,9 @@
+fun main() {
+  val dog = "Toto"
+  val state = "Kansas"
+  val movie = "The Wizard of Oz"
+
+
+  // Write your code below 🧙
+  println(dog + ", I've a feeling we're not in " + state + " anymore. - " + movie + ", 1939");
+}
