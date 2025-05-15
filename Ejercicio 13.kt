a6541c23eac5e68ea37d4ca9ff1296aaf243da5a
Sergio <spg147@inlumine.ual.es>
2025-05-15 13:08:25 +0200

    ejercicio para trabajar las String Templates

---
+++ Ejercicio 13.kt
@@ -0,0 +1,9 @@
+fun main() {
+  val plant = "orchid"
+  val potSize = 6 // in inches 
+  val dayNum = 7 
+
+  // Write your code below 🌱
+  println("An $plant in a $potSize inch pot must be watered every $dayNum days.");
+
+}
