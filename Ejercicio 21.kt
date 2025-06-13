132dbecbae98d0052a473b75358b8e735a240a2a
Sergio <spg147@inlumine.ual.es>
2025-06-13 12:23:22 +0200

    nuevo Ejercicio de operaciones aritmeticas en linea

---
+++ Ejercicio 21.kt
@@ -0,0 +1,29 @@
+fun main() {
+  var speedOfLight = 186_000
+  // Write your code here 
+  speedOfLight += 282
+
+  var perfectSquare = 32
+  // Write your code here 
+  perfectSquare *= 2
+
+  var sheldonsFavoriteNum = 219
+  // Write your code here 
+  sheldonsFavoriteNum /= 3
+
+  var emergency = 920 
+  // Write your code here 
+  emergency -= 9
+
+  var firstCountingNum = 10 
+  // Write your code here 
+  firstCountingNum %= 5
+
+
+   println("The speed of light is ${speedOfLight}.")
+  println("A perfect square is ${perfectSquare}.")
+  println("Sheldon's favorite number is ${sheldonsFavoriteNum}.")
+  println("The emergency number in the United States is ${emergency}.")
+  println("The first counting number in programming is ${firstCountingNum}.")
+
+}
