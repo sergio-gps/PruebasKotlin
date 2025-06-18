89e093d0de238f80bfe0f021edc74796a6e5bc47
Sergio <spg147@inlumine.ual.es>
2025-06-18 14:08:18 +0200

    Ejercicios para usar funciones matemáticas

---
+++ Ejercicio 22.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+  var a = 5 
+  a += 6
+
+  var b = a * 9 
+  var c = b / 3 
+  c++ 
+  
+  var d = c + 8 
+  d %= 4
+  d--
+  //d : 1
+  println(d)
+}
---
+++ Ejercicio 23.kt
@@ -0,0 +1,33 @@
+fun main() {
+
+  var ceil = Math.ceil(3.5)
+  println(ceil)
+  // Add your notes below
+  // puts the next integer value
+  var sqrt = Math.sqrt(25.0) 
+  println(sqrt)
+  // Add your notes below
+  // do de square root off the number
+  var abs = Math.abs(-20) 
+  println(abs)
+  // Add your notes below 
+  // absolute value of the number, no sign
+  var floor = Math.floor(6.9)
+  println(floor)
+  // Add your notes below 
+  // puts the lower integer
+
+/*
+The definitions for each function:
+
+    Math.ceil() accepts a Double value and returns the smallest value that is greater than or equal to the given value.
+
+    Math.sqrt() accepts a Double value and returns its square root.
+
+    Math.abs() accepts an Int value and returns its absolute value.
+
+    Math.floor() accepts a Double value and returns the largest value that is less than or equal to it.
+
+
+*/
+}
