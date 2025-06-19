a06a98f72bfe0e182b2cb9214c0ea397c2fa619c
Sergio <spg147@inlumine.ual.es>
2025-06-19 14:21:02 +0200

    final de unidad

---
+++ Ejercicio 24.kt
@@ -0,0 +1,18 @@
+/*Excellent work! You’ve completed the final lesson on
+Preview: Docs Variables are used to store a piece of data to be reused later in the program.
+variables
+and learned the fundamentals of numbers in Kotlin. Let’s review what was covered.
+
+    The Number superclass contains Int, Long, Short, Byte, Float and Double data types.
+    The arithmetic Operators are symbols or keywords used to perform operations on variables, values, or expressions in a program.
+    operators
+    supported by Kotlin include +, -, *, / and %.
+    Augmented assignment operators include an arithmetic and assignment operator used to calculate and reassign a value in one step.
+    The increment operator includes two consecutive plus operators, ++, used to add 1 to a value and reassign the result.
+    The decrement operator includes two consecutive minus operators, --, used to subtract one from a value and reassign the result.
+    The order of operations includes parentheses, multiplication, division, modulus, addition and subtraction.
+    The Math library is inherited from Java and contains various mathematical
+    Preview: Docs Loading link description
+    functions
+    for use in a Kotlin program.
+ */
