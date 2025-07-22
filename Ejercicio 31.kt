cd8ef5f18c2f5227225c3a2f16430f8e19cd65f1
Sergio <>
2025-07-22 14:51:40 +0200

    expresiones logicas

---
+++ Ejercicio 31.kt
@@ -0,0 +1,14 @@
+fun main() {
+  var expressionValue: Boolean
+  
+  if (!true && (false && true || !false)) {
+    println("Hello")
+  } else {
+    println("Goodbye")
+  }
+
+  // Write your code below
+  expressionValue = false
+  println(expressionValue)
+
+}
