a17b645492e8111e8a778712d2aa9d536cb4d083
Sergio <>
2025-07-24 14:54:50 +0200

    if else anidado

---
+++ Ejercicio 32.kt
@@ -0,0 +1,15 @@
+fun main() {
+  var orbitsStar = true // Rule 1
+  var hydrostaticEquilibrium = true // Rule 2
+  var clearedOrbit = false // Rule 3
+  
+  // Write your code below
+  if(orbitsStar && hydrostaticEquilibrium){
+    if(clearedOrbit){
+      println("Celestial body is a planet.")
+    } else {
+      println("Celestial body is a dwarf planet.")
+    }
+  }
+
+}
